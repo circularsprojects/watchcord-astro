ec40e7a1852325dfd040e73f8a6189754951fc60
circular <circular@circulars.dev>
2024-07-01 13:40:21 +1000

    add new badges

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,9 +4,51 @@ export default {
 	theme: {
 		extend: {
       fontFamily: {
-        display: 'Nunito Sans, ui-serif'
+        display: 'Inter, ui-serif'
       }
     },
 	},
-	plugins: [],
+  plugins: [
+    function ({ addUtilities, theme }) {
+      let backgroundSize = '7.07px 7.07px'
+      //let backgroundImage = (color) =>
+      //  `linear-gradient(135deg, ${color} 10%, transparent 10%, transparent 50%, ${color} 50%, ${color} 60%, transparent 60%, transparent 100%)`
+      let backgroundImage = (color) =>
+        `linear-gradient(135deg, transparent 10%, ${color} 10%, ${color} 50%, transparent 50%, transparent 60%, ${color} 60%, ${color} 100%)` 
+      let colors = Object.entries(theme('backgroundColor')).filter(
+        ([, value]) => typeof value === 'object' && value[400] && value[500]
+      )
+
+      addUtilities(
+        Object.fromEntries(
+          colors.map(([name, colors]) => {
+            let backgroundColor = colors[400] + '4D' // 10% opacity
+            let stripeColor = colors[500] + '80' // 50% opacity
+
+            return [
+              `.bg-stripes-${name}`,
+              {
+                backgroundColor,
+                backgroundImage: backgroundImage(stripeColor),
+                backgroundSize,
+              },
+            ]
+          })
+        )
+      )
+
+      addUtilities({
+        '.bg-stripes-white': {
+          backgroundImage: backgroundImage('rgba(255 255 255 / 0.8)'),
+          backgroundSize,
+        },
+      })
+
+      addUtilities({
+        '.ligatures-none': {
+          fontVariantLigatures: 'none',
+        },
+      })
+    },
+  ],
 }
